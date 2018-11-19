4f27e7569a7f76fedc950d96b2932fff127e1f34
alexander <alexchueshev@gmail.com>
2018-11-19 15:26:32 +0100

    template for java client

---
+++ src/main/java/com/example/client/CarRentalClient.java
@@ -0,0 +1,16 @@
+package com.example.client;
+
+import org.springframework.web.client.RestTemplate;
+
+public class CarRentalClient {
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        /**
+         * TODO: make requests to your server http://localhost:8080
+         * restTemplate.postForObject(..., ..., ...);
+         * restTemplate.getForObject(..., ...);
+         * */
+    }
+}
